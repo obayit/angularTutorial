0c009b4024011d014838dfd9db1accbc8ef26250
obi <obi@sudani.sd>
2018-10-28 16:37:38 +0200

    finished hero list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Jeremy', heroName: 'Scout' },
+  { id: 12, name: 'Jane Doe', heroName: 'Soldier' },
+  { id: 13, name: 'Unknown', heroName: 'Pyro' },
+  { id: 14, name: 'Tavish Finnegan DeGroot', heroName: 'Demoman' },
+  { id: 15, name: 'Mikhail', heroName: 'Heavy' },
+  { id: 16, name: 'Dell Conagher', heroName: 'Engineer' },
+  { id: 17, name: 'Ludwig Humboldt', heroName: 'Medic' },
+  { id: 18, name: 'Mun-dee', heroName: 'Sniper' },
+  { id: 19, name: 'Unknown', heroName: 'Spy' },
+];
